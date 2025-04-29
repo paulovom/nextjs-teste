be533d34186361cdd05b2633aa025bdceff52899
Paulo <paulo.melo@trustep.io>
2025-04-29 18:47:27 -0300

    Check Auth Issue

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,24 +5,25 @@ import { useSearchParams,
   usePathname,
   useRouter
  } from 'next/navigation';
+ import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-
-    console.log(term);
-  }
+  }, 300);
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
